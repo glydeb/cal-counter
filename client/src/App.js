cac5e63839626b77e6ab8ef612cf43990f0a74e6
Glydewell Burdick <glydewellb@gmail.com>
2023-03-30 22:16:16 -0500

    quickstart to app with contrived data hardcoded

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+
+const plates = [
+  {
+    id: 1,
+    title: "Chicken Dinner",
+    description: "Fried chicken with rice and salad",
+    calories: 500,
+  },
+  {
+    id: 2,
+    title: "Burger",
+    description: "Two all beef patties, special sauce, lettuce, cheese, pickles, onions on a sesame seed bun",
+    calories: 800,
+  },
+  {
+    id: 3,
+    title: "Balanced Breakfast",
+    description: "Scrambled eggs, toast, and fresh fruit",
+    calories: 400,
+  },
+  {
+    id: 4,
+    title: "Mini Clif Bar",
+    description: "Mini Clif Bar",
+    calories: 100,
+  },
+];
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewcalories: false,
+      activeItem: {
+        title: "",
+        description: "",
+        calories: 0
+      },
+      plateList: plates,
+    };
+  }
+
+  renderItems = () => {
+    const newItems = this.state.plateList
+
+    return newItems.map((item) => (
+      <li
+        key={item.id}
+        className="list-group-item d-flex justify-content-between align-items-center"
+      >
+        <span
+          className="plate-title mr-2"
+          title={item.description}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+          {item.title}
+        </span>
+        <span>
+          <button
+            className="btn btn-secondary mr-2"
+          >
+            Edit
+          </button>
+          <button
+            className="btn btn-danger"
+          >
+            Delete
+          </button>
+        </span>
+      </li>
+    ));
+  };
 
+  render() {
+    return (
+      <main className="container">
+        <h1 className="text-white text-uppercase text-center my-4">Calorie Counter app</h1>
+        <div className="row">
+          <div className="col-md-6 col-sm-10 mx-auto p-0">
+            <div className="card p-3">
+              <div className="mb-4">
+                <button
+                  className="btn btn-primary"
+                >
+                  Add plate
+                </button>
+              </div>
+              <ul className="list-group list-group-flush border-top-0">
+                {this.renderItems()}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </main>
+    );
+  }
+}
 export default App;
