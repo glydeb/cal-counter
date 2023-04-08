ec0aa4899e03d6b7adf5a4bc4f47caf583901753
Glydewell Burdick <glydewellb@gmail.com>
2023-04-08 17:23:18 -0500

    clean up hardcoded list and invisible heading 1 text

--- client/src/App.js
+++ client/src/App.js
@@ -2,33 +2,6 @@ import React, { Component } from "react";
 import Modal from "./components/Modal";
 import axios from "axios";
 
-const plates = [
-  {
-    id: 1,
-    title: "Chicken Dinner",
-    description: "Fried chicken with rice and salad",
-    calories: 500,
-  },
-  {
-    id: 2,
-    title: "Burger",
-    description: "Two all beef patties, special sauce, lettuce, cheese, pickles, onions on a sesame seed bun",
-    calories: 800,
-  },
-  {
-    id: 3,
-    title: "Balanced Breakfast",
-    description: "Scrambled eggs, toast, and fresh fruit",
-    calories: 400,
-  },
-  {
-    id: 4,
-    title: "Mini Clif Bar",
-    description: "Mini Clif Bar",
-    calories: 100,
-  },
-];
-
 class App extends Component {
   constructor(props) {
     super(props);
@@ -40,7 +13,7 @@ class App extends Component {
         calories: 0,
         time: new Date(),
       },
-      plateList: plates,
+      plateList: [],
     };
   }
 
@@ -74,7 +47,9 @@ class App extends Component {
   };
 
   handleDelete = (item) => {
-    alert("delete" + JSON.stringify(item));
+    axios
+      .delete(`http://localhost:8000/api/plates/${item.id}`)
+      .then((res) => this.refreshList());
   };
 
   createItem = () => {
@@ -121,7 +96,7 @@ class App extends Component {
   render() {
     return (
       <main className="container">
-        <h1 className="text-white text-uppercase text-center my-4">Calorie Counter app</h1>
+        <h1 className="text-blue text-uppercase text-center my-4">Calorie Counter app</h1>
         <div className="row">
           <div className="col-md-6 col-sm-10 mx-auto p-0">
             <div className="card p-3">
